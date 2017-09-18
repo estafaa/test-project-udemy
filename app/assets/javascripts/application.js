fcaa68bd6f66f85aafcd73f856811bd9639f9072
Anton <faa@phystech.edu>
2017-09-18 00:32:28 +0000

    create home page and link and footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
 
