40dd8e7a0fe558c145f92c592711a1660e8a5f7b
Anton <faa@phystech.edu>
2017-09-27 03:18:50 +0000

    create show action and gravatar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
-class UsersController < ArticlesController
+class UsersController < ApplicationController
     
     def new
     @user = User.new
     end
+    
+   
     
     def create
     @user = User.new(user_params)
@@ -14,10 +16,33 @@ class UsersController < ArticlesController
        end
     end
     
+     def edit
+    @user = User.find(params[:id])
+    end
+    
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated succesfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
+    def show
+    @user = User.find(params[:id])
+    
+   end
+
+    
+    
     private
     def user_params
-        params.require(:user).permit(:username,:email,:password)
+        params.require(:user).permit(:username, :email, :password)
     end
+   
     
     
 end
