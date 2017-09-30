90f452669c81e1eaac81f6a8fe3fe13e762c9e44
Anton <faa@phystech.edu>
2017-09-30 02:41:10 +0000

    added delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
     
-    before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
-    
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     
     def index
     @users = User.paginate(page: params[:page], per_page:5)
@@ -42,6 +42,12 @@ class UsersController < ApplicationController
     def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page:5)
    end
+   
+   def destroy
+       @user.destroy
+       flash[:danger] = "User and all his articles were deleted"
+       redirect_to users_path
+   end
 
     
     
@@ -55,12 +61,18 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
         flash[:danger] = "you can only edit your own account"
         redirect_to root_path
     end
     end
    
+    def require_admin
+        if logged_in? and !current_user.admin?
+            flash[:danger] = "Only admin can do this action"
+            redirect_to root_path
+        end
+    end
     
     
 end
